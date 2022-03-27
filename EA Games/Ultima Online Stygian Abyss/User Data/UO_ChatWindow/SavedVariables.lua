f6ba958523fd479dd6962f9f532ed69f1d8aef13
Andrew Kirk <scrubtasticx@gmail.com>
2022-03-27 13:18:09 -0400

    Initial Upload

---
+++ EA Games/Ultima Online Stygian Abyss/User Data/UO_ChatWindow/SavedVariables.lua
@@ -0,0 +1,181 @@
+ChatSettings.ChannelColors = 
+{
+	
+	{
+		b = 255,
+		g = 255,
+		r = 255,
+	},
+	
+	{
+		b = 1,
+		g = 215,
+		r = 215,
+	},
+	
+	{
+		b = 223,
+		g = 56,
+		r = 207,
+	},
+	
+	{
+		b = 230,
+		g = 120,
+		r = 75,
+	},
+	
+	{
+		b = 1,
+		g = 215,
+		r = 215,
+	},
+	
+	{
+		b = 1,
+		g = 215,
+		r = 215,
+	},
+	
+	{
+		b = 1,
+		g = 215,
+		r = 215,
+	},
+	
+	{
+		b = 1,
+		g = 215,
+		r = 215,
+	},
+	
+	{
+		b = 40,
+		g = 191,
+		r = 56,
+	},
+	
+	{
+		b = 0,
+		g = 231,
+		r = 96,
+	},
+	
+	{
+		b = 231,
+		g = 215,
+		r = 48,
+	}, 
+	[13] = 
+	{
+		b = 88,
+		g = 48,
+		r = 232,
+	},
+	[14] = 
+	{
+		b = 230,
+		g = 120,
+		r = 75,
+	},
+}
+
+
+
+ChatWindow.Settings = 
+{
+	Version = 1.5,
+	windowLocked = true,
+	autohide = false,
+	topWindow = "ChatWindow",
+	Chat = 
+	{
+		Windows = 
+		{
+			
+			{
+				posY = 0.7998046875,
+				alpha = 1,
+				windowMinAlpha = 0,
+				windowName = "ChatWindow",
+				sizeX = 485,
+				sizeY = 205,
+				movable = false,
+				showTimestamp = false,
+				windowMaxAlpha = 0.7,
+				posX = 0.0020874079782516,
+				numTabs = 1,
+				rootName = "ChatWindow",
+				Tabs = 
+				{
+					[1] = 
+					{
+						font = "UO_ChatText",
+						name = "System",
+						Filters = 
+						{
+							true,
+							false,
+							true,
+							true,
+							false,
+							true,
+							false,
+							false,
+							true,
+							true,
+							true, 
+							[13] = true,
+							[14] = true,
+						},
+					},
+				},
+				tabName = L"System",
+			},
+			
+			{
+				posY = 0.7998046875,
+				alpha = 1,
+				windowMinAlpha = 0,
+				windowName = "ChatWindow2",
+				sizeX = 485,
+				sizeY = 205,
+				movable = false,
+				showTimestamp = false,
+				windowMaxAlpha = 0.7,
+				posX = 0.0020874079782516,
+				numTabs = 1,
+				rootName = "ChatWindow",
+				Tabs = 
+				{
+					[1] = 
+					{
+						font = "UO_ChatText",
+						name = "Journal",
+						Filters = 
+						{
+							true,
+							true,
+							true,
+							true,
+							true,
+							true,
+							true,
+							true,
+							true,
+							true,
+							true, 
+							[13] = true,
+							[14] = true,
+						},
+					},
+				},
+				tabName = L"Journal",
+			},
+		},
+		numWindows = 2,
+	},
+}
+
+
+
